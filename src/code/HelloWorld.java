a841b5b6d0a5366c3c05f20bdd91190d570eae13
guoyuheng <1301201839@qq.com>
2021-11-15 18:04:50 +0800

    changed1

---
+++ src/code/HelloWorld.java
@@ -0,0 +1,15 @@
+package code;
+
+
+/**
+ * ClassName: HelloWorld
+ * Package: code
+ * Description:
+ * Date: 2021/11/15 18:03
+ * Author:1220024221.njupt.edu.cn
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
